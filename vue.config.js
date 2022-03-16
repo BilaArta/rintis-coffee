ebfaeae64d32d77f24d7e3a92eac8394da94ecf5
BilaArta <bilaarta@gmail.com>
2022-03-15 18:19:38 -0700

    gh-pages CI

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 
 module.exports = defineConfig({
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/',
   transpileDependencies: true,
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/Project-Web-Desa-Client-Side/'
